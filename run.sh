df2ac9fade8f23b469cfe59d4ca61f26ad262239
riantkb <kubota.rian@gmail.com>
2019-10-23 14:11:11 +0900

    modify script

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ if [ $? -ne 0 ] ; then
     echo "Exit due to Concatenation Error." >&2
     exit
 fi
-mcs submittable.txt -out:Program.exe
+mcs submittable.txt -out:Program.exe >&2
 if [ $? -ne 0 ] ; then
     echo "Exit due to Compilation Error." >&2
     exit
 fi
-echo "Compilation was successful." >&2
+echo "Compilation succeeded." >&2
 mono Program.exe < /dev/stdin
 # cat submittable.txt | pbcopy
