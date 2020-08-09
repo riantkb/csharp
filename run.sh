9b6a4348826c33152835f218b832e030f3452d08
riantkb <kubota.rian@gmail.com>
2020-08-09 15:36:07 +0900

    change compiler

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if [ $? -ne 0 ] ; then
     echo "Exit due to Concatenation Error." >&2
     exit
 fi
-mcs submittable.txt -out:Program.exe >&2
+csc submittable.txt -out:Program.exe >&2
 if [ $? -ne 0 ] ; then
     echo "Exit due to Compilation Error." >&2
     exit
